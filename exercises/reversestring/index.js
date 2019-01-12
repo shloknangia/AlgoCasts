fdc7f1a4257ca2514ca5f7b6465762fc244c2bec
shlok nangia <shloknangia@gmail.com>
2019-01-12 17:51:40 +0530

    reverse string sol2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,7 +6,7 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
+function reverse_1(str) { //1
 
 	let revStr = '';
 	let l = str.length;
@@ -14,6 +14,24 @@ function reverse(str) {
 		revStr += str[l - i - 1];
 	}
 	return revStr;
+}
+
+function reverse(str) { //2
+
+	// const arr = str.split('');
+	// arr.reverse();
+	// return arr.join('');
+	return str.split('').reverse().join('');
+}
+
+
+function reverse(str) { //3
+
+	let rev = '';
+	for (let c of str){
+		rev = c + rev;
+	}
+	return rev;
 }
 
 module.exports = reverse;
---
+++ play.js
@@ -0,0 +1,7 @@
+
+str = "apple";
+let rev = '';
+for (let c of str){
+	rev = c + rev;
+}
+console.log(rev);
